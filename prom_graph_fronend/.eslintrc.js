e63630ad2d3438ed3c53e34e73379d7069d6d465
Exlau <285874446@qq.com>
2022-12-22 17:58:16 +0800

    feat: 🎸 Set up ant-design

--- prom_graph_fronend/.eslintrc.js
+++ prom_graph_fronend/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
     'plugin:react/recommended',
@@ -11,7 +12,26 @@ module.exports = {
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+    'import/extensions': 0,
   },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
   rules: {
     'react/jsx-filename-extension': [2,
       {
@@ -20,5 +40,7 @@ module.exports = {
     ],
     'linebreak-style': 0,
     semi: ['error', 'never'],
+    'react/jsx-uses-react': 2,
+    'import/extensions': 'off',
   },
 }
